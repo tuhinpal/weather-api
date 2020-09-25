32b491a660f482b1a2d833c67049004433f59146
Tuhin Kanti Pal <me@mailtuhin.ml>
2020-09-25 17:34:15 +0530

    Vercel

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const OAuth = require('oauth');
+module.exports = async(req, res) => {
+    var location = req.query.query;
+    var header = {
+        "X-Yahoo-App-Id": process.env.YAAHOO_APP_ID
+    };
+    var requestweather = new OAuth.OAuth(
+        null,
+        null,
+        process.env.YAAHOO_CLIENT_ID,
+        process.env.YAAHOO_CLIENT_SECRET,
+        '1.0',
+        null,
+        'HMAC-SHA1',
+        null,
+        header
+    );
+    requestweather.get(
+        'https://weather-ydn-yql.media.yahoo.com/forecastrss?location=' + location + '&format=json&u=c',
+        null,
+        null,
+        function(err, data, result) {
+            if (err) {
+                res.send('An Error Occurred' + (err))
+            } else {
+                var jsondata = JSON.parse(data);
+                res.status(200).send(jsondata)
+            }
+        }
+    );
+}
